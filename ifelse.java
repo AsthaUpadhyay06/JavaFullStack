57264af0b7b3c7d4caf12e9df0f0a5ddddc12014
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-10-30 18:20:40 +0530

    ifelse statement

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+public class ifelse {
+    public static void main(String[] args) {
+        boolean isStudent=true;
+        boolean isSenior;
+        double price=45.5666;
+        if (isStudent){
+            price +=0.4;
+            
+        }
+        else{
+            price *=1;
+        }
+System.out.println("student price is "+ price);
+
+    }
+}
