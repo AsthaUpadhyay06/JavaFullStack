58fd29192ead55fa0d00662394ce3abf6986f0aa
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-10-30 18:32:56 +0530

    nested if

--- ifelse.java
+++ ifelse.java
@@ -1,14 +1,28 @@
 public class ifelse {
     public static void main(String[] args) {
-        boolean isStudent=true;
-        boolean isSenior;
-        double price=45.5666;
+        boolean isStudent=false;
+        boolean isSenior=false;
+        int price=45;
         if (isStudent){
-            price +=0.4;
+            if(isSenior){
+                System.out.println("agar upar vala chala toh ye chalega");
+           price +=4;
+            }
             
+            else{
+                System.out.println("abhi nhi pta kya hoga");
+                price +=3;
+            }
         }
         else{
-            price *=1;
+            if(isSenior){
+                System.out.println("else vala if  chalega");
+                price +=2;
+            }
+            else{
+                System.out.println("nhhi to else chaslegs");
+                price +=1;
+            }
         }
 System.out.println("student price is "+ price);
 
