be9c8a462eddff94a714b3795933e775de8e3f7a
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-10-30 19:56:33 +0530

    ternaryoperator

---
+++ ternaryoperator.java
@@ -0,0 +1,14 @@
+public class ternaryoperator {
+    public static void main(String[] args) {
+        //variable=(condition)? ifTrue :ifFalse
+     int score=70;
+    String passOrFail=(score>=60) ? "pass":"fail";
+    System.out.println(passOrFail);
+
+int num=5;
+String evenorodd=(num%2==0) ? "even":   "odd";
+System.out.println(evenorodd);
+}
+    }
+
+
