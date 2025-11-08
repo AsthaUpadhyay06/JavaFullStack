8090046658d2bf04b05a3fcaea2309c482592b04
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-11-08 19:17:38 +0530

    constructors

---
+++ constructors.java
@@ -0,0 +1,78 @@
+//     class MyEmployee{
+//         private int id ;
+//         private String name;
+//         public MyEmployee( int id,String name){
+//           this. id=id;
+//            this. name=name;
+//         }  
+//          public  String getName(){
+//            return name;
+//          }
+//          public  void setName(String n){
+//            name=n;
+//          }
+//          public int  getId(){
+//            return id;
+//          }
+//          public  void setID(int i){
+//             id=i;
+//          }
+
+
+//     }
+  
+// public class constructors {
+//     public static void main(String[] args) {
+//         MyEmployee obj=new MyEmployee(34,"astha");
+//         // obj.setName("astha");
+//         // obj.setID(45);
+//         System.out.println(obj.getId());
+//         System.out.println(obj.getName());
+        
+//     }
+// }
+    
+
+
+// class Employee {
+//     private int salary;
+//     private String name;
+
+//     // Constructor 1 (user se salary aur name dono lega)
+//     Employee(int s, String n) {
+//         salary = s;
+//         name = n;
+//     }
+
+//     // Constructor 2 (salary fix 10000, sirf name input hoga)
+//     Employee(String n) {
+//         salary = 10000; // default / fixed salary
+//         name = n;
+//     }
+
+//     public int getSalary() {
+//         return salary;
+//     }
+
+//     public String getName() {
+//         return name;
+//     }
+// }
+
+// public class constructors {
+
+//     public static void main(String[] args) {
+
+//         Employee e1 = new Employee(25000, "Astha"); // pehla constructor
+//         System.out.println(e1.getName() + " : " + e1.getSalary());
+
+//         Employee e2 = new Employee("Aditi"); // dusra constructor
+//         System.out.println(e2.getName() + " : " + e2.getSalary());
+//     }
+// }
+
+
+
+   
+
+ 
