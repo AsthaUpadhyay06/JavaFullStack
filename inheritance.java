ef206336c472be09dad4626f1a54463369a1568e
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-11-10 17:51:12 +0530

    Inheritance

---
+++ inheritance.java
@@ -0,0 +1,71 @@
+//  class Base{
+//     int x;
+
+//     public void setx(int x){
+//         this.x=x;
+//     }
+//     public int getx(){
+//         return x;
+//  }
+//   public void printMe(){
+//     System.out.println("print me iam base class");
+//   }
+// }
+
+// class Derived extends Base{
+//     int y;
+//      public void sety(int y){
+//         this.y=y;
+//     }
+//     public int gety(){
+//         return y;
+//  }
+//   public void printMee(){
+//     System.out.println("print me iam derived class");
+//   }
+// }
+
+
+// public class inheritance {
+//     public static void main(String[] args) {
+//         Base obj=new Base();
+//         obj.setx(34);
+//         System.out.println(obj.getx());
+//         Derived obj1=new Derived();
+//         obj1.sety(35);
+//         System.out.println(obj1.gety());
+//         obj.printMe();
+//         obj1.printMe();
+//         obj1.printMee();
+//     }
+// }
+
+
+ class Animal{
+    String name;
+    int age;
+     
+    public void animal( String n,int a){
+        name=n;
+        age=a;
+        System.out.println("name " + name + " age " + age);
+       
+    }
+ }
+
+ class Dog extends  Animal{
+    public void dog(){
+        System.out.println("barkingggg");
+    }
+ }
+public class inheritance{
+    public static void main(String[] args) {
+        Animal obj=new Animal();
+        obj.animal("Rabit", 3);
+
+        Dog obj1=new Dog();
+        obj1.animal("dog", 4);
+        obj1.dog();
+
+    }
+}
