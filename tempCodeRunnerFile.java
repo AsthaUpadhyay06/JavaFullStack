d5ecad0bf7fca39a16c2562d51c066db9bdab471
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-11-13 00:03:50 +0530

    thread

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,25 +1,32 @@
-class Phone {
-     public void music(){
-         System.out.println("muisc.....");
-     }
-     public void listening(){
-        System.out.println("listeninggg");
-     }
- }
- class smartphone extends Phone{
-    public void listenmusic(){
-         System.out.println("muisc.....");
-     }
-     public void listening(){
-        System.out.println("listeninggg music  ");
-     }
- }
-public class inheritanceconstructor{
-    public static void main(String[] args) {
-         Phone p1=new smartphone();
-         p1.listening();
-         p1.music();
-        //  p1.listeningmusic(); // not allowed
-        
+class MyThreadRunnable extends Thread {
+    public void run() {
+        int i = 0;
+        while (i < 4000) {
+            System.out.println("My first thread is running");
+            i++;
+        }
     }
 }
+
+class MyThreadRunnable1 extends Thread {
+    public void run() {
+        int i = 0;
+        while (i < 4000) {
+            System.out.println("My second thread is running");
+            i++;
+        }
+    }
+}
+
+public class ThreadExample {  // renamed from 'Thread'
+    public static void main(String[] args) {
+        MyThreadRunnable t1 = new MyThreadRunnable();
+        MyThreadRunnable1 t2 = new MyThreadRunnable1();
+
+        t1.start();  // start thread t1
+        t2.start();  // start thread t2
+    }
+}
+
+
+
