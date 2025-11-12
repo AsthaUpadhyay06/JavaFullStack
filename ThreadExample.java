6e1a4d519d1172eaef3989a10c9a883759db8767
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-11-12 23:41:34 +0530

    Thread

---
+++ ThreadExample.java
@@ -0,0 +1,33 @@
+
+//  class MyThreadRunnable extends Thread {
+//     public void run() {
+//         int i = 0;
+//         while (i < 4000) {
+//             System.out.println("My first thread is running");
+//             i++;
+//         }
+//     }
+// }
+
+// class MyThreadRunnable1 extends Thread {
+//     public void run() {
+//         int i = 0;
+//         while (i < 4000) {
+//             System.out.println("My second thread is running");
+//             i++;
+//         }
+//     }
+// }
+
+// public class ThreadExample {  // renamed from 'Thread'
+//     public static void main(String[] args) {
+//         MyThreadRunnable t1 = new MyThreadRunnable();
+//         MyThreadRunnable1 t2 = new MyThreadRunnable1();
+
+//         t1.start();  // start thread t1
+//         t2.start();  // start thread t2
+//     }
+// }
+
+
+
---
+++ problem4.java
@@ -0,0 +1,29 @@
+
+class Library{
+    String[] books;
+    int noofbooks=0;
+    Library(){
+        this.books=new String[100];
+        this.noofbooks=0;
+    }
+    public void addBook(String BOOK){
+        System.out.println("add book in library");
+    }
+    public void issueBook(){
+
+    }
+    public void returnBook(){
+
+    }
+    public void showAvailable(){
+
+    }
+ 
+    
+}
+
+public class problem4 {
+    public static void main(String[] args) {
+        
+    }
+}
