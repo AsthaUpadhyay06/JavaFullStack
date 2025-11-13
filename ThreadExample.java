45d6b0507028182b83b09d87209e9dd05f51b27d
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-11-13 11:22:42 +0530

    Thread Constructor

--- ThreadExample.java
+++ ThreadExample.java
@@ -1,33 +1,90 @@
 
- class MyThreadRunnable extends Thread {
-    public void run() {
-        int i = 0;
-        while (i < 4000) {
-            System.out.println("My first thread is running");
-            i++;
+//  class MyThreadRunnable extends Thread {
+//     public void run() {
+//         int i = 0;
+//         while (i < 4000) {
+//             System.out.println("My first thread is running");
+//             i++;
+//         }
+//     }
+// }
+
+// class MyThreadRunnable1 extends Thread {
+//     public void run() {
+//         int i = 0;
+//         while (i < 4000) {
+//             System.out.println("My second thread is running");
+//             i++;
+//         }
+//     }
+// }
+
+// public class ThreadExample {  // renamed from 'Thread'
+//     public static void main(String[] args) {
+//         MyThreadRunnable t1 = new MyThreadRunnable();
+//         MyThreadRunnable1 t2 = new MyThreadRunnable1();
+
+//         t1.start();  // start thread t1
+//         t2.start();  // start thread t2
+//     }
+// }
+
+
+
+// runnable thread
+
+// class MyThreadRunnable implements Runnable {
+//     public void run() {
+//         int i = 0;
+//         while (i < 4000) {
+//             System.out.println("My first thread is running");
+//             i++;
+//         }
+//     }
+// }
+
+// class MyThreadRunnable1 implements Runnable {
+//     public void run() {
+//         int i = 0;
+//         while (i < 4000) {
+//             System.out.println("My second thread is running");
+//             i++;
+//         }
+//     }
+// }
+
+// public class ThreadExample {
+//     public static void main(String[] args) {
+//         MyThreadRunnable t1 = new MyThreadRunnable();
+//         MyThreadRunnable1 t2 = new MyThreadRunnable1();
+
+//         Thread gun1 = new Thread(t1);
+//         Thread gun2 = new Thread(t2);
+
+//         gun1.start();
+//         gun2.start();
+//     }
+// }
+
+
+  class astha extends Thread{
+    public astha(String name){
+    super(name);
+    }
+    public void run(){
+        int i=34;
+        System.out.println("thankyou");
+        while(true){
+            System.out.println("run thread");
         }
     }
-}
-
-class MyThreadRunnable1 extends Thread {
-    public void run() {
-        int i = 0;
-        while (i < 4000) {
-            System.out.println("My second thread is running");
-            i++;
-        }
-    }
-}
-
-public class ThreadExample {  // renamed from 'Thread'
+  }
+  
+ public class ThreadExample{
     public static void main(String[] args) {
-        MyThreadRunnable t1 = new MyThreadRunnable();
-        MyThreadRunnable1 t2 = new MyThreadRunnable1();
-
-        t1.start();  // start thread t1
-        t2.start();  // start thread t2
+        astha a1=new astha("upadhyaya");
+        a1.start();
+        System.out.println("the id of threasd is  " + a1.getId());
+        System.out.println("then name " + a1.getName());
     }
-}
-
-
-
+ }
