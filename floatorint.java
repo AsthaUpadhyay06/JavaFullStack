6bef5c162b2fe138f11116107c1858c2a61e5d4b
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-11-16 23:55:38 +0530

    ageindays

---
+++ floatorint.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class floatorint {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String  X=sc.next();
+        if(X.contains(".") ){
+            System.out.println("");
+        }
+       
+       
+    }
+}
