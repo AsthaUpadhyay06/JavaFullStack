233909149d18c05ae2231bdaac9886ab56ca6422
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-11-30 23:55:04 +0530

    codee

---
+++ W.java
@@ -0,0 +1,30 @@
+ import java.util.*;
+
+public class W {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        char S = sc.next().charAt(0);  // + - *
+        int B = sc.nextInt();
+        char eq = sc.next().charAt(0); // '=' ignore
+        int C = sc.nextInt();
+
+        int result = 0;
+
+        if (S == '+') {
+            result = A + B;
+        } else if (S == '-') {
+            result = A - B;
+        } else if (S == '*') {
+            result = A * B;
+        }
+
+        if (result == C) {
+            System.out.println("Yes");
+        } else {
+            System.out.println(result);
+        }
+    }
+}
+ 
